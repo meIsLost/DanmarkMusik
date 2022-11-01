f24d4f571f3c9241dcd774bd9a3c77fa7d366ffe
Sabin Bhandari <sabi5403@edu.easj.dk>
2022-11-01 14:40:27 +0100

    Did everything basically

---
+++ DanmarkMusik/Controllers/MusikRecordController.cs
@@ -0,0 +1,95 @@
+﻿using DanmarkMusik.Managers;
+using DanmarkMusik.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DanmarkMusik.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusikRecordController : ControllerBase
+    {
+        private readonly MusikRecordManager _manager= new MusikRecordManager();        // GET: api/<RecordMusikController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<IEnumerable<MusikRecord>> Get()
+        {
+           IEnumerable<MusikRecord> musikList =_manager.GetAll();
+            if (musikList.Count() == 0)
+            {
+                return NoContent();
+            }
+            return Ok(musikList);
+        }
+
+        // GET api/<RecordMusikController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<MusikRecord> Get(int id)
+        {
+            MusikRecord? result = _manager.GetById(id);
+            if (result == null)
+                return NotFound();  
+            else
+                return Ok(result);
+        }
+
+        // POST api/<RecordMusikController>
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [HttpPost]
+        public ActionResult<MusikRecord>Post([FromBody] MusikRecord value)
+        {
+            try
+            {
+                MusikRecord result = _manager.Add(value);
+                return Created("api/footballplayers/" + result.Id, result);
+            }
+            catch (Exception ex)
+            when (ex is ArgumentNullException || ex is ArgumentOutOfRangeException)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<RecordMusikController>/5
+        [HttpPut("{id}")]
+        public ActionResult<MusikRecord> Put(int id, [FromBody] MusikRecord value)
+        {
+            try 
+            {
+                MusikRecord? result = _manager.Update(id, value);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                else
+                    return Ok(result);
+            }
+            catch (Exception ex)
+                when (ex is ArgumentNullException || ex is ArgumentOutOfRangeException)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+         
+        }
+
+        // DELETE api/<RecordMusikController>/5
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpDelete("{id}")]
+        public ActionResult<MusikRecord> Delete(int id)
+        {
+            MusikRecord? result = _manager.Delete(id);
+            if (result != null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ DanmarkMusik/Managers/MusikRecordManager.cs
@@ -0,0 +1,58 @@
+﻿
+using DanmarkMusik.Models;
+
+namespace DanmarkMusik.Managers
+{
+    public class MusikRecordManager
+    {
+       
+        
+            private static int _nextId = 1;
+
+            private static readonly List<MusikRecord> Data = new List<MusikRecord>
+        {
+            new MusikRecord { Id = _nextId++, Artist = "The Script", Duration = 300, Title = "Hall of Fame", PublicationYear= 2012},
+            new MusikRecord { Id = _nextId++, Artist = "Denmark", Duration = 210, Title = "Zealand", PublicationYear= 2010},
+            new MusikRecord { Id = _nextId++, Artist = "Andres", Duration = 120, Title = "JavaScript", PublicationYear= 2022},
+            new MusikRecord { Id = _nextId++, Artist = "The Script", Duration = 180, Title = "Paint the town green", PublicationYear= 2011}
+        };
+
+            public List<MusikRecord> GetAll()
+            {
+                return new List<MusikRecord>(Data);
+            }
+
+            public MusikRecord? GetById(int id)
+            {
+                return Data.Find(music => music.Id == id);
+            }
+
+            public MusikRecord Add(MusikRecord newMusikRecord)
+            {
+                newMusikRecord.Id = _nextId++;
+                Data.Add(newMusikRecord);
+                return newMusikRecord;
+            }
+
+            public MusikRecord? Delete(int id)
+            {
+                MusikRecord? music = Data.Find(music1 => music1.Id == id);
+                if (music == null) return null;
+                Data.Remove(music);
+                return music;
+            }
+
+            public MusikRecord? Update(int id, MusikRecord updates)
+            {
+                MusikRecord? music = Data.Find(music1 => music1.Id == id);
+                if (music == null) return null;
+
+                music.Title = updates.Title;
+                music.Artist = updates.Artist;
+                music.Duration = updates.Duration;
+                music.PublicationYear = updates.PublicationYear;
+                return music;
+            }
+    }
+    
+}
